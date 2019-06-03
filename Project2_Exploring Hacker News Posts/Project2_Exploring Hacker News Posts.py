adba054049a35ddca3fcfdd6adc557f7129ae022
kay kang <kkang1119@outlook.com>
2019-06-03 09:52:29 -0400

    data science projects

---
+++ Project2_Exploring Hacker News Posts/Project2_Exploring Hacker News Posts.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ASK HN vs. SHOW HN Comments
+# 
+# We are going to compare two different types of posts(Ask HN or Show HN) to see wich posts receives more comments on average. We are also interested in if posts created at a certain time receive more comments on average.
+
+# In[1]:
+
+
+from csv import reader
+
+opened_file = open('hacker_news.csv')
+read_file = reader(opened_file)
+hn = list(read_file)
+hn[:5]
+
+
+# In[2]:
+
+
+# extract the first row of data
+headers = hn[0]
+# remove the headers from the list
+hn = hn[1:]
+
+print(headers)
+print(hn[:5])
+
+
+# In[3]:
+
+
+ask_posts = [] #list to hold 'ask hn' posts
+show_posts = [] #list to hold 'show hn' posts
+other_posts = [] #list to hold all other posts
+
+# for loop to append specific posts to its matching lists
+for post in hn:
+    title = post[1]
+    if title.lower().startswith('ask hn'):
+        ask_posts.append(post)
+    elif title.lower().startswith('show hn'):
+        show_posts.append(post)
+    else:
+        other_posts.append(post)
+        
+print('ask_posts has a total of', len(ask_posts), 'posts')
+print('show_posts has a total of', len(show_posts), 'posts')
+print('show_posts has a total of', len(other_posts), 'posts')
+
+
+# In[4]:
+
+
+# Calculate the average number of comments `Ask HN` posts receive.
+total_ask_comments = 0
+
+for post in ask_posts:
+    total_ask_comments += int(post[4])
+    
+avg_ask_comments = total_ask_comments / len(ask_posts)
+print('Ask posts receive average of', avg_ask_comments, 'comments')
+
+# Calculate the average number of comments 'Show HN' posts receive
+total_show_comments = 0
+for post in show_posts:
+    total_show_comments += int(post[4])
+
+avg_show_comments = total_show_comments / len(show_posts)
+print('Show posts receive average of', avg_show_comments, 'comments')
+
+
+# In average, ask posts receive more comments. Ask posts receive a total of about 14 comments per post while the show posts receive an average of about 10 comments per post.
+
+# ## Finding the Amount of Ask Posts and Comments by Hour Created
+# We will analyze how many posts were posted during a speicific time and how many comments those posts received. 
+
+# In[5]:
+
+
+#import datetime module as dt
+import datetime as dt
+import operator
+
+result_list = []
+
+#append created_at column and comments column to result_list[]
+#as a list[] with two elements
+for post in ask_posts:
+    created = post[6]
+    comments = int(post[4])
+    result_list.append([created, comments])
+    
+posts_by_hour = {}
+comments_by_hour = {}
+date_format = "%m/%d/%Y %H:%M"
+# loop through result_list[] and parse the date to create a datetime object
+for row in result_list:
+    created_time = row[0]
+    comments = row[1]
+    # extract the %H(Hour) from the datetime object
+    created_hour = dt.datetime.strptime(created_time, date_format).strftime("%H")
+    if created_hour in posts_by_hour:
+        posts_by_hour[created_hour] += 1
+        comments_by_hour[created_hour] += comments
+    else:
+        posts_by_hour[created_hour] = 1
+        comments_by_hour[created_hour] = comments
+
+print('Number of Posts During a Speicific Hour')
+print('------------------------------------------')
+print(sorted(zip(posts_by_hour.keys(), posts_by_hour.values())))
+print('\n')
+
+print('Number of Comments During a Speicific Hour')
+print('------------------------------------------')
+print(sorted(zip(comments_by_hour.keys(), comments_by_hour.values())))
+
+
+# ## Calculate Average Number of Comments by the Hour for Ask HN Posts
+
+# In[9]:
+
+
+avg_by_hour = []
+for hour in comments_by_hour:
+    avg_by_hour.append([hour, comments_by_hour[hour] / posts_by_hour[hour]])
+
+#sort the list using the itemgetter method from the operator module
+print(sorted(avg_by_hour, key=operator.itemgetter(1), reverse=True))
+
+
+# We will sort the above list using a different method to swap positions of the elements to make it easier to identify the hours with the highest values.
+
+# In[10]:
+
+
+swap_avg_by_hour = []
+for row in avg_by_hour:
+    swap_avg_by_hour.append([row[1], row[0]])
+sorted_swap = sorted(swap_avg_by_hour, reverse=True)
+print(sorted_swap)
+
+
+# In[14]:
+
+
+print("Top 5 Hours for Ask Posts Comments")
+for avg, hr in sorted_swap[:5]:
+    template = '{hr}: {avg:.2f} average comments per post'
+    output = template.format(hr=dt.datetime.strptime(hr, "%H").strftime("%H:%M"), avg=avg)
+    print(output)
+
+
+# ## Conclusion
+# From the above analysis, it seems like the most comments per post is received around 15:00 or 3:00 p.m. with an average of 38.59 comments per post. Overall, it seems like the best timeframe to post to receive the most comments is between 3-4 p.m. as these two timeframe receives total of 45 comments on average. If the post can't be made during this timeframe, the second best option will be around 8-9p.m. as it receives a total of about 38 comments on average.
+# 
---
+++ Project3_Ebay_Car_Sales/p3_ebay_car_sales.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Used Car Data from eBay Kleinanzeigen
+#
+# The aim of this project is to clean the data and analyze the included used car listing.
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+autos = pd.read_csv(
+    "autos.csv", encoding="Windows-1252")
+
+
+# Print the autos data to inspect the data
+
+# In[2]:
+
+
+autos
+
+
+# Use DataFrame.info() and DataFrame.head() methods to print information about the autos dataframe, as well as the first few rows.
+
+# In[3]:
+
+
+autos.info()
+autos.head()
+
+
+# Inspecting the data using the DataFrame.info() method, we can see that there are some columns with massing values. The columns with missing values are 'vehicleType', 'gearbox', 'model', 'fuelType', 'notRepairedDamage'. The autos dataframe has 5 columns with int data type and 15 columns with object data type.
+
+# # Clean Columns
+
+# In[4]:
+
+
+autos.columns
+
+
+# In[5]:
+
+
+autos.columns = ['date_crawled', 'name', 'seller', 'offer_type', 'price', 'abtest',
+                 'vehicle_type', 'registration_year', 'gearbox', 'power_ps', 'model',
+                 'odometer', 'registration_month', 'fuel_type', 'brand',
+                 'unrepaired_damage', 'ad_created', 'nr_of_pictures', 'postal_code',
+                 'last_seen']
+autos.head()
+
+
+# Renamed multiple columns after inspecting the original column names using 'autos.columns'. All the camelcase names have been switched to snakecase. Column yearOfRegistration was renamed to registration_year. COlumn monthOfRegistration to registration_month. Column notRepairedDamage to unrepaired_damage. Column dateCreated to ad_created.
+
+# # Initial Data Exploration and Cleaning
+
+# In[6]:
+
+
+autos.describe(include='all')
+
+
+# The columns seller and offer_type seem to have the same values for all rows as it only 2 unique values. nr_of_pictures column is returning value of 0.0 for most statistics. postal_code and registration_year column also seems to need further investigation as the unique and top rows are missing values. price and odometer columns are numeric values stored as text.
+
+# In[7]:
+
+
+print(autos['seller'].value_counts())
+print()
+print(autos['offer_type'].value_counts())
+print()
+print(autos['nr_of_pictures'].value_counts())
+print()
+print(autos['postal_code'].value_counts().head())
+print()
+print(autos['registration_year'].value_counts().head())
+print()
+print(autos['price'].head())
+print()
+print(autos['odometer'].head())
+
+
+# In[8]:
+
+
+autos = autos.drop(["nr_of_pictures", "seller", "offer_type"], axis=1)
+autos.columns
+
+
+# Removed nr_of_pictures, seller, and offer_type columns from the dataframe because they either had 2 unique values or one values for all columns.
+
+# # Cleaning Odometer and Price
+
+# In[9]:
+
+
+autos['price'] = (autos['price']
+                  .str.replace('$', '')
+                  .str.replace(',', '')
+                  .astype(int)
+                  )
+autos['odometer'] = (autos['odometer']
+                     .str.replace('km', '')
+                     .str.replace(',', '')
+                     .astype(int)
+                     )
+autos.rename({'odometer': 'odometer_km'}, axis=1, inplace=True)
+print(autos['price'].head())
+print(autos['odometer_km'].head())
+print(autos.columns)
+
+
+# Changed the text values to numeric values in the price and odometer column. Renamed the odometer column to odometer_km to specify the unit it is in.
+
+# In[10]:
+
+
+for column in ['odometer_km', 'price']:
+    print(autos[column].unique().shape)
+    print(autos[column].describe())
+    print()
+    print(autos[column].value_counts().sort_index(ascending=False).head(20))
+    print()
+
+
+# In[11]:
+
+
+autos = autos[autos['price'].between(1, 351000)]
+autos['price'].describe()
+
+
+# Removed outliers that cost less than $1 and greater than $350000. The prices gradually increases upto 350000 and suddently jumps up which is suspicious. Although $1 car prices aren't realistic, because ebay is an auction site, there could be junk cars that are sold for that price.
+
+# # Exploring the Date Columns
+
+# In[12]:
+
+
+autos[['date_crawled', 'ad_created', 'last_seen']][0:5]
+
+
+# In[13]:
+
+
+print(autos['date_crawled']
+      .str[:10]
+      .value_counts(normalize=True, dropna=False)
+      .sort_index()
+      )
+
+
+# In[14]:
+
+
+print(autos['date_crawled']
+      .str[:10]
+      .value_counts(normalize=True, dropna=False)
+      .sort_values(ascending=False)
+      )
+
+
+# The site was crawled daily for about a month. The distribution is roughly normal.
+
+# In[15]:
+
+
+print(autos['last_seen']
+      .str[:10]
+      .value_counts(normalize=True, dropna=False)
+      .sort_index())
+
+
+# In[16]:
+
+
+print(autos['last_seen']
+      .str[:10]
+      .value_counts(normalize=True, dropna=False)
+      .sort_values(ascending=False))
+
+
+# The last seen values includes listings of cars that were removed because it was sold. The values have gradually increased until the last date of crawling. It indicates that the auction period was probably for about a month until it was sold off or the listing for the auction has ended. There are least chance of the car getting sold off. The cars sale goes up as the the auction gets close to the end or the listing ends because of the duration limit of the ebay.
+
+# In[17]:
+
+
+print(autos['ad_created']
+      .str[:10]
+      .value_counts(normalize=True, dropna=False)
+      .sort_index())
+
+
+# ad_created dates span for about a year. It seems like most of the ads are created during March and falls off during other months.
+
+# # Dealing with Incorrect Registration Year Data
+
+# In[18]:
+
+
+autos['registration_year'].describe()
+
+
+# There are odd values within the registration_year column. Year 1000 was before any cars were even created and year 9999 is thounsands of years later in to the future
+
+# In[19]:
+
+
+autos['registration_year'].describe()
+
+
+# In[20]:
+
+
+print((autos["registration_year"].between(1900, 2019)).sum() / autos.shape[0])
+(~autos["registration_year"].between(1900, 2019)).sum() / autos.shape[0]
+
+
+# Given that the registration year between 1900 and 2016 is close to 100% of the data, it seems to be safe to remove anything beyond that.
+
+# In[21]:
+
+
+autos.loc[(autos['registration_year'] < 1900), 'registration_year']
+
+
+# In[22]:
+
+
+autos.loc[(autos['registration_year'] > 2019), 'registration_year']
+
+
+# After reviewing the registration year, it seems reasonable to remove the cars that has the registration year prior to 1900 becaue the cars were invented in the late 1800s. Also because the current year is 2019, cars registered after this year can be removed as it is not possible to have a registration year of the future.
+
+# In[23]:
+
+
+autos = autos[autos['registration_year'].between(1900, 2019)]
+autos['registration_year'].value_counts(normalize=True).head(50).sort_index()
+
+
+# It seems that vehical registration started booming starting in the early 1990s. Late 1990s and early 2000s are where we see the most registration of vehicles.
+
+# # Exploring Price by Brand
+
+# In[24]:
+
+
+autos['brand'].value_counts(normalize=True)
+
+
+# It seems like the most popular brands are european car brands. Volkswagen is by far the most popular brand. There are brands with such a small value of percentage so we will limit our analysis to brands representing more than 5%.
+
+# In[25]:
+
+
+brand_counts = autos['brand'].value_counts(normalize=True)
+common_brands = brand_counts[brand_counts > .05].index
+print(common_brands)
+
+
+# In[26]:
+
+
+brand_mean_prices = {}
+
+for brand in common_brands:
+    brand_only = autos[autos['brand'] == brand]
+    mean = brand_only['price'].mean()
+    brand_mean_prices[brand] = int(mean)
+
+brand_mean_prices
+
+
+# Of the most 5 popular brands, it seems like Audi, BMW, and Benz are the most expensive. Volkswagen has a middle ground price among these top brands which suggests that consumers prefer the cars that are priced in a middle.
+
+# # Exploring Mileage
+
+# In[28]:
+
+
+brand_mean_mileage = {}
+
+for brand in common_brands:
+    brand_only = autos[autos['brand'] == brand]
+    mean_mileage = brand_only['odometer_km'].mean()
+    brand_mean_mileage[brand] = int(mean_mileage)
+
+brand_mean_mileage
+
+
+# In[42]:
+
+
+mean_price = pd.Series(brand_mean_prices).sort_values(ascending=False)
+mean_mileage = pd.Series(brand_mean_mileage).sort_values(ascending=False)
+print(mean_price)
+print(mean_mileage)
+
+
+# Converted the dictionaries average price and mileage to pandas Series by using the consturctor. The converted series will be entered into a new dataframe 'brand_info'.
+
+# In[44]:
+
+
+brand_info = pd.DataFrame(mean_price, columns=['mean_price'])
+brand_info
+
+
+# In[43]:
+
+
+brand_info['mean_mileage'] = mean_mileage
+brand_info
+
+
+# It seems like the average mileage is about similar among all brands but there are big price differences. Even though Ford and Opel have similar or even less mileage than the top 3 brands, they are far less expensive.
